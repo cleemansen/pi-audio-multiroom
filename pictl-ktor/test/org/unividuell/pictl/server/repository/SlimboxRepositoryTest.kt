7f7379bb57a9baba1589c2598977c4c5c723f582
Clemens Hahn <clemens@couchbits.com>
2020-12-16 10:26:54 +0100

    WIP: test setup

---
+++ pictl-ktor/test/org/unividuell/pictl/server/repository/SlimboxRepositoryTest.kt
@@ -0,0 +1,12 @@
+package org.unividuell.pictl.server.repository
+
+import kotlin.test.Test
+import kotlin.test.fail
+
+internal class SlimboxRepositoryTest {
+
+    @Test
+    fun `it should map the current song info`() {
+        fail()
+    }
+}
