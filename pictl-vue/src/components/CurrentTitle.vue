eebf9cee8114042ce09e057f9f73b3eff0a560dd
Clemens Hahn <clemens@couchbits.com>
2021-03-26 11:19:11 +0100

    extract view component of currently played title

---
+++ pictl-vue/src/components/CurrentTitle.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p class="display-1 text--primary">{{ currentSong }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CurrentTitle",
+  props: ['artist', 'title'],
+  data: () => ({}),
+  computed: {
+    currentSong() {
+      let currentSong = ""
+      if (this.artist) {
+        currentSong += this.artist
+      }
+      if (this.title) {
+        if (currentSong !== "") {
+          currentSong += " — "
+        }
+        currentSong += this.title
+      }
+      return currentSong
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
