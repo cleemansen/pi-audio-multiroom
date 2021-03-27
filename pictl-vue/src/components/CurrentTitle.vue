95a7b42d88aece05d1a03431f653cdc35388bc4c
Clemens Hahn <clemens@couchbits.com>
2021-03-27 12:47:37 +0100

    a first approach splitting current title by word

--- pictl-vue/src/components/CurrentTitle.vue
+++ pictl-vue/src/components/CurrentTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="display-1 text--primary">{{ currentSong }}</p>
+    <p id="current-title" class="display-1 text--primary">{{ currentSong }}</p>
   </div>
 </template>
 
@@ -8,7 +8,9 @@
 export default {
   name: "CurrentTitle",
   props: ['artist', 'title'],
-  data: () => ({}),
+  data: () => ({
+    selection: null
+  }),
   computed: {
     currentSong() {
       let currentSong = ""
@@ -23,10 +25,51 @@ export default {
       }
       return currentSong
     }
+  },
+  mounted() {
+    document.onselectionchange = () => {
+      let selection = document.getSelection()
+      console.log(selection)
+      if (selection.focusOffset != null) {
+        let context = selection.focusNode.textContent
+        let selectedWord = "";
+        // backwards to first word-splitter
+        for (let i = selection.focusOffset - 1; i >= 0; i--) {
+          let currentChar = context[i]
+          if (currentChar === ' ') {
+            break
+          }
+          selectedWord += context[i]
+        }
+        selectedWord = selectedWord.split("").reverse().join("")
+        // forwards to first word-splitter
+        for (let i = selection.focusOffset; i < context.length; i++) {
+          let currentChar = context[i]
+          if (currentChar === ' ') {
+            break
+          }
+          selectedWord += context[i]
+        }
+        console.log(selectedWord)
+
+        // let span = document.createElement("span")
+        // span.setAttribute("class", "lookup")
+        // span.appendChild(document.createTextNode(selectedWord))
+        // document.getElementById("current-title").insertBefore(span, selection.focusNode.splitText(selection.focusOffset))
+      }
+    }
+  },
+  methods: {},
+  watch: {
+    selection: function (newVal) {
+      console.log("changed to " + newVal)
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.lookup {
+  color: aquamarine;
+}
 </style>
