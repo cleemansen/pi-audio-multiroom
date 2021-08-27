ceef151364f0fba7c648ec529bc6d050d5b0178d
Clemens Hahn (couchbits GmbH) <clemens@couchbits.com>
2021-08-27 14:50:53 +0200

    revert trust-all-tls-certs workaround for cometd-client (we use now real and valid LE certs)

--- pictl-ktor/src/org/unividuell/pictl/server/network/cometd/SqueezeboxBayeuxClient.kt
+++ pictl-ktor/src/org/unividuell/pictl/server/network/cometd/SqueezeboxBayeuxClient.kt
@@ -14,13 +14,46 @@ import java.security.cert.X509Certificate
 import javax.net.ssl.SSLContext
 import javax.net.ssl.X509TrustManager
 
-class SqueezeboxBayeuxClient : KoinComponent {
+interface SqueezeboxBayeuxClient {
+    fun buildBayeuxClient(): BayeuxClient
+}
+
+class SqueezeboxBayeuxDefaultClient : SqueezeboxBayeuxClient, KoinComponent {
 
     private val application: Application by inject()
 
     private val slimserverHost = application.environment.config.property("ktor.application.slimserver.host").getString()
 
-    fun buildBayeuxClient(): BayeuxClient {
+    override fun buildBayeuxClient(): BayeuxClient {
+        val logging = HttpLoggingInterceptor(CometOkHttpLogger())
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(SqueezeboxCometConnectPatchInterceptor())
+//            .addNetworkInterceptor(SqueezeboxCometGzipPatchInterceptor())
+            .addNetworkInterceptor(logging)
+            .build()
+
+        // The maximum number of milliseconds to wait before considering a request to the LMS failed
+        val longPollingTimeout = 30_123  // an odd number to uniquely identify timeouts by this option
+        val options = mutableMapOf<String, Any>()
+        options[HttpClientTransport.MAX_NETWORK_DELAY_OPTION] = longPollingTimeout
+        val jsonContext = JacksonJSONContextClient()
+        options[HttpClientTransport.JSON_CONTEXT_OPTION] = jsonContext
+        val httpTransport = OkHttpClientTransport(options, httpClient)
+
+        return BayeuxClient("$slimserverHost/cometd", httpTransport)
+    }
+
+}
+
+class SqueezeboxBayeuxTrustEveryTlsCertClient : SqueezeboxBayeuxClient, KoinComponent {
+
+    private val application: Application by inject()
+
+    private val slimserverHost = application.environment.config.property("ktor.application.slimserver.host").getString()
+
+    override fun buildBayeuxClient(): BayeuxClient {
         val logging = HttpLoggingInterceptor(CometOkHttpLogger())
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
 
@@ -32,8 +65,10 @@ class SqueezeboxBayeuxClient : KoinComponent {
             .addInterceptor(SqueezeboxCometConnectPatchInterceptor())
 //            .addNetworkInterceptor(SqueezeboxCometGzipPatchInterceptor())
             .addNetworkInterceptor(logging)
+            // trust all SSL/TLS certificates. this is normally not needed as we use real and valid LE certificates
             .sslSocketFactory(sslContext.socketFactory, trustEveryCerts)
             .hostnameVerifier { _, _ -> true }
+            // END trust all SSL/TLS certificates.
             .build()
 
         // The maximum number of milliseconds to wait before considering a request to the LMS failed
