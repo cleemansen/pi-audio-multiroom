1244609c64bc7288c3b2973b1acb025e862b38ff
Clemens Hahn (couchbits GmbH) <clemens@couchbits.com>
2021-08-28 11:00:02 +0200

    abort deploy-script on errors in subshells // housekeeping

--- deploy-to-white.sh
+++ deploy-to-white.sh
@@ -1,10 +1,20 @@
-# build frontend
+#!/usr/bin/env bash
+# Deploy the whole app to the target environment
+#
+# Best practices: https://kvz.io/bash-best-practices.html
+# exit when a command fails.
+set -o errexit
+# exit when your script tries to use undeclared variables
+set -o nounset
+
+target="${1}"
+
+echo "build frontend"
 (cd pictl-vue && npx browserslist@latest --update-db && ./node_modules/.bin/vue-cli-service build --dest ../pictl-ktor/resources/pictl-vue/)
-# bundle backend (includes frontend)
+echo "bundle backend (with bundled frontend)"
 (cd pictl-ktor && ./mvnw clean package)
-# deploy to server
-(cd pictl-ktor && scp target/pictl-ktor-1.0.0-jar-with-dependencies.jar pi@white.unividuell.org:pictl.jar)
-# restart pictl
-ssh pi@white.unividuell.org 'sudo systemctl restart pictl'
-# done
+echo "deploy to $target"
+(cd pictl-ktor && scp target/pictl-ktor-1.0.0-jar-with-dependencies.jar pi@"${target}":pictl.jar)
+echo "restart pictl on $target"
+ssh pi@"${target}" 'sudo systemctl restart pictl'
 echo 'deployed :)'
