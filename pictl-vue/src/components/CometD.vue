28c0069a64548ece579b7fc8e966b13f2fd1489b
clemens <cleemansen@gmail.com>
2021-12-21 15:49:24 +0100

    a working example for connecting to LMS by cometd-client

--- pictl-vue/src/components/CometD.vue
+++ pictl-vue/src/components/CometD.vue
@@ -1,77 +1,166 @@
 <template>
-  <div>Connected: {{ connected }}</div>
+  <div>
+    <h1>CometD client status</h1>
+    <ul>
+      <li>connected: {{this.connected}}</li>
+      <li>player-id: {{this.playerId}}</li>
+      <li v-if="cometD">client: {{cometD.getStatus()}} {{cometD.getClientId()}}</li>
+    </ul>
+    <v-btn @click="connect">connect</v-btn>
+    <v-btn @click="subscribePlayer">subscribe</v-btn>
+    <textarea id="log" readonly cols="80" rows="25" placeholder="Log"></textarea>
+  </div>
 </template>
 
 <script>
-import {CometD} from 'cometd'
-import {adapt} from 'cometd-nodejs-client';
+import { CometD } from 'cometd'
+import { adapt } from 'cometd-nodejs-client';
 
 export default {
   name: "CometD",
   data: () => ({
-    cometd: null,
-    connected: false
+    cometD: null,
+    connected: false,
+    playerId: null
   }),
   mounted() {
     adapt()
-    this.cometd = new CometD()
     console.debug = console.log
-    this.connect()
   },
   methods: {
-    log(message) {
-      console.log(`app: ${message}`)
+    log(text) {
+      let log = document.getElementById("log");
+      log.setRangeText(text + "\n");
+      log.scrollTop = log.scrollHeight;
+      log.setSelectionRange(log.value.length, log.value.length);
     },
-    connect() {
-      if (this.cometd) {
-        // init is equal to configure + handshake
-        this.cometd.init({
-          url: 'https://lms.unividuell.org/cometd',
-          // useWorkerScheduler: false,
-          logLevel: "debug"
-        })
-        this.cometd.addListener("/meta/connect", (message) => {
-          this.log(this.connected ? "connected" : "connection failed");
-          if (this.connected !== message.successful) {
-            this.connected = message.successful;
-          }
-        });
-        this.cometd.addListener("/meta/handshake", (message) => {
-          if (message.successful) {
-            this.log("handshake ok");
-            this.cometd.subscribe(`/${this.cometd.getClientId()}/**`, (message) => this.handleCometDMessage(message));
-            this.cometd.subscribe("/slim/subscribe", () => {},
-                {
-                  data: {
-                    response: `/${this.cometd.getClientId()}/slim/serverstatus`,
-                    request: ["", ["serverstatus", 0, 999, "subscribe:0"]]
-                  }
-                }
-            );
-          } else {
-            this.log("handshake failed");
-          }
-        });
-      } else {
-        this.log(`already connected.`)
+
+    clearLog() {
+      let log = document.getElementById("log");
+      log.setSelectionRange(0, log.value.length);
+      log.setRangeText("");
+    },
+
+    checkConnected() {
+      if (!this.cometD) {
+        this.log("Not connected");
+        return false;
       }
+      if (!this.connected) {
+        this.log("Connection failed");
+        return false;
+      }
+      return true;
     },
+
+    checkPlayer() {
+      if (!this.checkConnected()) {
+        return false;
+      }
+      if (!this.playerId) {
+        this.log("no player");
+        return false;
+      }
+      return true;
+    },
+
     handleCometDMessage(message) {
       this.log(message.channel);
       if (message.channel.endsWith("/slim/serverstatus")) {
         // Set this.playerId to that of the first player
-      //   if (message.data && message.data.players_loop && message.data.players_loop.length > 0) {
-      //     let playerId = message.data.players_loop[0].playerid;
-      //     let name = message.data.players_loop[0].name;
-      //     if (this.playerId !== playerId) {
-      //       if (!!this.playerId) {
-      //         this.unsubscribePlayer();
-      //         this.log(`player ${this.playerId} unsubscribed`);
-      //       }
-      //       this.playerId = playerId;
-      //       this.log(`player set to ${this.playerId} (${name})`);
-      //     }
-      //   }
+        if (message.data && message.data.players_loop && message.data.players_loop.length > 0) {
+          let playerId = message.data.players_loop[0].playerid;
+          let name = message.data.players_loop[0].name;
+          if (this.playerId !== playerId) {
+            if (this.playerId) {
+              this.unsubscribePlayer();
+              this.log(`player ${this.playerId} unsubscribed`);
+            }
+            this.playerId = playerId;
+            this.log(`player set to ${this.playerId} (${name})`);
+          }
+        }
+      }
+    },
+
+    connect() {
+      if (!this.cometD) {
+        this.cometD = new CometD();
+        console.debug = console.log;
+        this.cometD.unregisterTransport('websocket');
+
+        this.cometD.init({
+          url: (process.env.NODE_ENV === 'production' ? location.origin : "https://lms.unividuell.org") + "/cometd",
+          useWorkerScheduler: false,
+          logLevel: "debug"
+        });
+
+        this.cometD.addListener("/meta/connect", (message) => {
+          if (this.connected !== message.successful) {
+            this.connected = message.successful;
+            this.log(this.connected ? "connected" : "connection failed");
+          }
+        });
+
+        this.cometD.addListener("/meta/handshake", (message) => {
+          if (message.successful) {
+            this.log("handshake ok");
+            this.cometD.subscribe(`/${this.cometD.getClientId()}/**`, (message) => this.handleCometDMessage(message));
+            this.cometD.subscribe("/slim/subscribe", (msg) => {this.log(msg)},
+                {
+                  data: {
+                    response: `/${this.cometD.getClientId()}/slim/serverstatus`,
+                    request: ["", ["serverstatus", 0, 999, "subscribe:0"]]
+                  }
+                }
+            );
+          }
+          else {
+            this.log("handshake failed");
+          }
+        });
+        this.log("cometD is now initialized")
+      }
+      else {
+        this.log("already connected");
+      }
+    },
+
+    disconnect() {
+      if (this.cometD) {
+        this.cometD.disconnect(
+            (message) => {
+              this.log("disconnect:", message.channel);
+            });
+        this.cometD = undefined;
+        this.connected = undefined;
+        this.playerId = undefined;
+      }
+    },
+
+    subscribePlayer() {
+      if (this.checkPlayer()) {
+        this.cometD.subscribe("/slim/subscribe", () => {},
+            {
+              data: {
+                response: `/${this.cometD.getClientId()}/slim/playerstatus/${this.playerId}`,
+                request: [this.playerId, ["status", "-", 1, "subscribe:0"]]
+              }
+            }
+        );
+      }
+    },
+
+    unsubscribePlayer() {
+      if (this.checkPlayer()) {
+        this.cometD.subscribe("/slim/subscribe", () => {},
+            {
+              data: {
+                response: `/${this.cometD.getClientId()}/slim/playerstatus/${this.playerId}`,
+                request: [this.playerId, ["status", "-", 1, "subscribe:-"]]
+              }
+            }
+        );
       }
     }
   }
