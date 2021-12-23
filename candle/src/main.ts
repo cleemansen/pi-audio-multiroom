0e9955af2ec7e8a30a113c055053877460a17f72
clemens <cleemansen@gmail.com>
2021-12-23 16:10:10 +0100

    setup antd and first usage of an antd component

--- candle/src/main.ts
+++ candle/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import "ant-design-vue/dist/antd.css";
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(Antd).mount('#app')
