3a94a6c0bf0d130826093b7e5b99ffb82287897a
clemens <cleemansen@gmail.com>
2021-12-28 15:35:18 +0100

    rm antd

--- candle/src/components/HelloAntDesign.vue
+++ candle/src/components/HelloAntDesign.vue
@@ -1,26 +1,6 @@
 <template>
   <div style="background-color: #ececec; padding: 20px">
-    <a-row :gutter="16" type="flex" justify="center">
-      <a-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8" style="background-color: #00acc1">
-        <a-page-header
-            style="border: 1px solid rgb(235, 237, 240)"
-            :ghost="false"
-            title="Title"
-            sub-title="This is a subtitle">
-          <a-row class="content">
-            <div style="flex: 1">
-              <h1>{{ store.currentTitle }}</h1>
-              <p>Hello from the content section!</p>
-              <img src="../assets/logo.png" />
-            </div>
-          </a-row>
-        </a-page-header>
-      </a-col>
-      <a-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8" style="background-color: #00b0ff">
-        <a-card :loading="loading" title="2. Card">whatever content</a-card>
-        <a-button style="margin-top: 16px" @click="handleClick">Toggle loading</a-button>
-      </a-col>
-    </a-row>
+
   </div>
 
 </template>
--- candle/src/main.ts
+++ candle/src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
-import "ant-design-vue/dist/antd.css";
 import { createPinia } from "pinia";
 import App from './App.vue'
 
 createApp(App)
     .use(createPinia())
-    .use(Antd)
     .mount('#app')
