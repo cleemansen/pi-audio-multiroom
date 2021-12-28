07036d73112feddb4283b599d58d7b7f64768173
clemens <cleemansen@gmail.com>
2021-12-28 16:44:54 +0100

    parse the LMS artwork-url

--- candle/src/store/LmsStore.ts
+++ candle/src/store/LmsStore.ts
@@ -18,7 +18,7 @@ export const useLmsStore = defineStore('playerStatus', {
                 state.players[playerIdx].title = playerCometD.remoteMeta?.title
                 state.players[playerIdx].artist = playerCometD.remoteMeta?.artist
                 state.players[playerIdx].remoteTitle = playerCometD.remoteMeta?.remote_title
-                state.players[playerIdx].artworkUrl = playerCometD.remoteMeta?.artwork_url
+                state.players[playerIdx].artworkUrl = parseArtworkUrl(playerCometD.remoteMeta?.artwork_url)
                 state.players[playerIdx].mode = playerCometD.mode
                 state.players[playerIdx].mixerVolume = playerCometD["mixer volume"]
                 state.players[playerIdx].connected = playerCometD.player_connected === 1
@@ -37,4 +37,14 @@ export const useLmsStore = defineStore('playerStatus', {
         }
     }
 })
+
+function parseArtworkUrl(lmsArtworkUrl?: string) {
+    if (!lmsArtworkUrl) return lmsArtworkUrl
+
+    if (lmsArtworkUrl.startsWith("/imageproxy/") || lmsArtworkUrl.startsWith("/plugins/")) {
+        return `https://lms.unividuell.org` + lmsArtworkUrl
+    } else {
+        return lmsArtworkUrl
+    }
+}
 
