4636a698111fbe794cc4ed430805c0df907c6b27
clemens <cleemansen@gmail.com>
2021-12-28 15:52:35 +0100

    add vuestic ui

--- candle/src/main.ts
+++ candle/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from "pinia";
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 import App from './App.vue'
 
 createApp(App)
     .use(createPinia())
+    .use(VuesticPlugin)
     .mount('#app')
