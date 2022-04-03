789f1a4c889afea612116ca8ad11ff96a29cddb2
clemens <cleemansen@gmail.com>
2022-04-03 14:08:11 +0200

    housekeeping

--- candle/src/main.ts
+++ candle/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
 loadFonts();
 
