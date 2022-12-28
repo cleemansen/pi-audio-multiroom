85e13a170b022f7351319e67b4c08a27a7bc92cc
clemens <cleemansen@gmail.com>
2022-12-28 13:29:23 +0100

    update dependencies

--- candle/vite.config.ts
+++ candle/vite.config.ts
@@ -2,10 +2,13 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vuetify from "@vuetify/vite-plugin";
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
