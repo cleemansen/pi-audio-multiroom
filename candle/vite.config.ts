929494afacc4415a0008ce52587da2e253b60479
clemens <cleemansen@gmail.com>
2023-04-22 12:17:12 +0200

    simplify build/tooling config

--- candle/vite.config.ts
+++ candle/vite.config.ts
@@ -2,12 +2,12 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vuetify from 'vite-plugin-vuetify';
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
   plugins: [
     vue(),
