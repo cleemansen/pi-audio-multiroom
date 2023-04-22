7f5f066e58e0bfac40f097daee909a68f759f8cd
clemens <cleemansen@gmail.com>
2023-04-22 11:30:36 +0200

    migrate to composition api

--- candle/src/components/PlayerVolume.vue
+++ candle/src/components/PlayerVolume.vue
@@ -1,20 +1,4 @@
 <template>
-  <!-- wait for https://github.com/vuetifyjs/vuetify/issues/14936
-  <v-progress-linear
-    v-bind:key="playerId"
-    v-model="vol"
-    :stream="desiredVolume >= 0"
-    height="14"
-    color="indigo"
-    class="mt-1 elevation-4"
-  >
-    <template v-slot:default="{ value }">
-      <div class="pt-2 pb-2" style="color: white; font-size: 14px">
-        {{ playerName }} {{ Math.ceil(value) }}
-      </div>
-    </template>
-  </v-progress-linear>
-  -->
   <v-slider
     v-bind:key="playerId"
     v-model="vol"
@@ -26,59 +10,48 @@
   </v-slider>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
 
-export default defineComponent({
-  props: { playerId: String, playerName: String, mixerVolume: Number },
-  setup() {
-    const loading = ref(false);
-    const desiredVolume = ref(-1);
+const props = defineProps({
+  playerId: String,
+  playerName: String,
+  mixerVolume: Number,
+});
+const emit = defineEmits(["desired-volume"]);
 
-    return {
-      loading,
-      desiredVolume,
-    };
-  },
-  computed: {
-    vol: {
-      get: function () {
-        return this.mixerVolume;
-      },
-      set: function (newValue: number) {
-        this.desiredVolume = Math.ceil(newValue);
-      },
-    },
-    progress: {
-      get: function () {
-        return this.desiredVolume >= 0;
-      },
-      set: function () {
-        // no-op
-      },
-    },
-    stateColor: {
-      get: function () {
-        return this.progress ? "purple" : "indigo";
-      },
-      set: function () {
-        // no-op
-      },
-    },
-  },
-  watch: {
-    desiredVolume: function (val: number) {
-      if (val) {
-        this.$emit("desired-volume", this.playerId, val);
-      }
-    },
-    mixerVolume: function (val: number) {
-      if (this.desiredVolume === val) {
-        this.desiredVolume = -1;
-      }
-    },
+const desiredVolume = ref(-1);
+
+const vol = computed({
+  get: () => props.mixerVolume,
+  set: (newValue: number) => (desiredVolume.value = Math.ceil(newValue)),
+});
+const progress = computed({
+  get: () => desiredVolume.value >= 0,
+  set: () => {
+    // no-op
   },
 });
+const stateColor = computed({
+  get: () => (progress.value ? "purple" : "indigo"),
+  set: () => {
+    // no-op
+  },
+});
+
+watch(desiredVolume, (val: number) => {
+  if (val) {
+    emit("desired-volume", props.playerId, val);
+  }
+});
+watch(
+  () => props.mixerVolume,
+  (val: number) => {
+    if (desiredVolume.value === val) {
+      desiredVolume.value = -1;
+    }
+  }
+);
 </script>
 
 <style scoped></style>
