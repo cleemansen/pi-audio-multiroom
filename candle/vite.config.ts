1f5a1a1fdfc9302382a61a1cb759bf6d08b9c74a
clemens <cleemansen@gmail.com>
2025-08-17 13:31:39 +0200

    add PWA support with VitePWA plugin and include base app icon

--- candle/vite.config.ts
+++ candle/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,50 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+      },
+      includeAssets: ["public/favicon.ico"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Candle - Multiroom Audio",
+        short_name: "Candle",
+        description: "Multiroom Audio Control App",
+        theme_color: "#F7930E",
+        background_color: "#0F1525",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
   define: { "process.env": {} },
